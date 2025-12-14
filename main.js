c8c848bb4c6524cb0abe35a7b2ede0c86c014593
•astraea-oss• <astraea.oss@gmail.com>
2025-12-14 15:47:16 +0000

    Switching to the css method

--- main.js
+++ main.js
@@ -25,39 +25,10 @@ __export(main_exports, {
 module.exports = __toCommonJS(main_exports);
 var import_obsidian = require("obsidian");
 var HeaderModifierPlugin = class extends import_obsidian.Plugin {
-  async onload() {
+  onload() {
     console.log("Header Modifier Plugin loaded");
-    this.addStyle();
   }
   onunload() {
     console.log("Header Modifier Plugin unloaded");
-    const style = document.getElementById("header-modifier-style");
-    if (style) style.remove();
-  }
-  addStyle() {
-    const style = document.createElement("style");
-    style.id = "header-modifier-style";
-    style.innerHTML = `
-            /* Markdown preview headers */
-            .markdown-preview-view h1,
-            .markdown-preview-view h2,
-            .markdown-preview-view h3,
-            .markdown-preview-view h4,
-            .markdown-preview-view h5,
-            .markdown-preview-view h6 {
-                font-size: 1em !important;
-            }
-
-            /* Editor headers (CodeMirror 6) */
-            .cm-header-1,
-            .cm-header-2,
-            .cm-header-3,
-            .cm-header-4,
-            .cm-header-5,
-            .cm-header-6 {
-                font-size: 1em !important;
-            }
-        `;
-    document.head.appendChild(style);
   }
 };
--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,11 @@
 import { Plugin } from 'obsidian';
 
 export default class HeaderModifierPlugin extends Plugin {
-    async onload() {
+    onload() {
         console.log('Header Modifier Plugin loaded');
-        this.addStyle();
     }
 
     onunload() {
         console.log('Header Modifier Plugin unloaded');
-        const style = document.getElementById('header-modifier-style');
-        if (style) style.remove();
-    }
-
-    addStyle() {
-        const style = document.createElement('style');
-        style.id = 'header-modifier-style';
-        style.innerHTML = `
-            /* Markdown preview headers */
-            .markdown-preview-view h1,
-            .markdown-preview-view h2,
-            .markdown-preview-view h3,
-            .markdown-preview-view h4,
-            .markdown-preview-view h5,
-            .markdown-preview-view h6 {
-                font-size: 1em !important;
-            }
-
-            /* Editor headers (CodeMirror 6) */
-            .cm-header-1,
-            .cm-header-2,
-            .cm-header-3,
-            .cm-header-4,
-            .cm-header-5,
-            .cm-header-6 {
-                font-size: 1em !important;
-            }
-        `;
-        document.head.appendChild(style);
     }
 }
